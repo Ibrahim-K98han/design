2e23752e112e7ee0e84aa950c6c6cfe2a5eac940
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-11-16 22:23:22 +0600

    error end

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'components/home_screen_navbar.dart';
 import 'components/lists/explore_course_list.dart';
 import 'components/lists/recent_course_list.dart';
-import 'model/course.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +29,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
 
   late Animation<Offset> sidebarAnimation;
+  late Animation<double> fadeAnimation;
   late AnimationController sidebarAnimationController;
+
+  var sidebarHidden = true;
 
   @override
   void initState() {
@@ -48,6 +50,21 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
         curve: Curves.easeInOut
       )
     );
+    fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0
+    ).animate(
+        CurvedAnimation(
+            parent: sidebarAnimationController,
+            curve: Curves.easeInOut
+        )
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    sidebarAnimationController.dispose();
   }
 
   @override
@@ -58,56 +75,85 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
         child: Stack(
           children: [
             SafeArea(
-              child: Column(
-                children: [
-                  HomeScreenNavBar(triggerAnimation: (){
-                    sidebarAnimationController.forward();
-                  },),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          'Recents',
-                          style: kLargeTitleStyle,
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          '23  Corses, more coming',
-                          style: kSubtitleStyle,
-                        ),
-                      ],
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    HomeScreenNavBar(
+                      triggerAnimation: (){
+                        setState(() {
+                          sidebarHidden = sidebarHidden;
+                        });
+                      sidebarAnimationController.forward();
+                    },),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            'Recents',
+                            style: kLargeTitleStyle,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            '23  Corses, more coming',
+                            style: kSubtitleStyle,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const RecentCouseList(),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 20, right: 20, top: 25, bottom: 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          'Explore',
-                          style: kTitle1Style,
-                        )
-                      ],
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  ExploreCourseList(),
-                ],
+                    const RecentCouseList(),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, top: 25, bottom: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            'Explore',
+                            style: kTitle1Style,
+                          )
+                        ],
+                      ),
+                    ),
+                    ExploreCourseList(),
+                  ],
+                ),
               ),
             ),
-            SlideTransition(
-              position: sidebarAnimation,
-              child: SafeArea(
-                  child: SidebarScreen(),
-                  bottom: false,
+            IgnorePointer(
+              ignoring: sidebarHidden,
+              child: Stack(
+                children: [
+                  FadeTransition(
+                    opacity: fadeAnimation,
+                    child: GestureDetector(
+                      child: Container(
+                        color: Color.fromRGBO(36, 38, 41, 0.4),
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                      onTap: (){
+                        setState(() {
+                          sidebarHidden = sidebarHidden;
+                        });
+                        sidebarAnimationController.reverse();
+                      },
+                    ),
+                  ),
+                  SlideTransition(
+                    position: sidebarAnimation,
+                    child: SafeArea(
+                      bottom: false,
+                      child: SidebarScreen(),
+                    ),
+                  )
+                ],
               ),
             ),
           ],
